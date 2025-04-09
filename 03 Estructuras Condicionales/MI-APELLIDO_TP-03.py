7ad04c0668e2df09bb74a9b776bdd96a11aafc4a
Nahuel <nahueldgo@gmail.com>
2025-04-09 03:16:43 -0300

    En proceso

--- 03 Estructuras Condicionales/MI-APELLIDO_TP-03.py
+++ 03 Estructuras Condicionales/MI-APELLIDO_TP-03.py
@@ -1,14 +1,9 @@
-##7) Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado
-## termina con vocal, añadir un signo de exclamación al final e imprimir el string resultante por
-## pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por
-## pantalla.
-
-
-cadena_de_texto = str(input("Escriba una frase o una palabra: "))
-
-if cadena_de_texto[-1] =="a" or cadena_de_texto[-1] =="e" or cadena_de_texto[-1] =="i" or cadena_de_texto[-1] =="o" or cadena_de_texto[-1] =="u":
-    print ("La ultima letra es vocal")
-else:
-    print("La ultima letra no es vocal")
-
-
+## 9) Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la
+## magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado por pantalla:
+##
+## ● Menor que 3: "Muy leve" (imperceptible).
+## ● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
+## ● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero generalmente no causa daños).
+## ● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras débiles).
+## ● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
+## ● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
