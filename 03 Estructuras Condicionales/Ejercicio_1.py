d72973469d54e1bdc84822314662d6115d4d99cf
Nahuel <nahueldgo@gmail.com>
2025-04-09 23:47:43 -0300

    Ejercicio listo

---
+++ 03 Estructuras Condicionales/Ejercicio_1.py
@@ -0,0 +1,8 @@
+##1) Escribir un programa que solicite la edad del usuario. Si el usuario es mayor de 18 años,
+## deberá mostrar un mensaje en pantalla que diga “Es mayor de edad”.
+
+edad = int(input("Ingrese su edad: "))
+if edad >= 18:
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
