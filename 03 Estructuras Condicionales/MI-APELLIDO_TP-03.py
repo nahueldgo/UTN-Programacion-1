faacc71aa07957739bfc16e6e6e459d6ee298fa5
Nahuel <nahueldgo@gmail.com>
2025-04-09 22:46:11 -0300

    Trabajando en el ultimo ejercicio

--- 03 Estructuras Condicionales/MI-APELLIDO_TP-03.py
+++ 03 Estructuras Condicionales/MI-APELLIDO_TP-03.py
@@ -1,23 +1,4 @@
-## 9) Escribir un programa que pida al usuario la magnitud de un terremoto, clasifique la
-## magnitud en una de las siguientes categorías según la escala de Richter e imprima el resultado por pantalla:
-##
-## ● Menor que 3: "Muy leve" (imperceptible).
-## ● Mayor o igual que 3 y menor que 4: "Leve" (ligeramente perceptible).
-## ● Mayor o igual que 4 y menor que 5: "Moderado" (sentido por personas, pero generalmente no causa daños).
-## ● Mayor o igual que 5 y menor que 6: "Fuerte" (puede causar daños en estructuras débiles).
-## ● Mayor o igual que 6 y menor que 7: "Muy Fuerte" (puede causar daños significativos).
-## ● Mayor o igual que 7: "Extremo" (puede causar graves daños a gran escala).
-
-magnitud = int(input("Ingrese la magnitud de un terremoto para clasificarlo: "))
-if magnitud<3:
-    print("Muy leve")
-elif magnitud>=3 and magnitud<4:
-    print("Leve")
-elif magnitud>=4 and magnitud<5:
-    print("Moderado")
-elif magnitud>=5 and magnitud<6:
-    print("Fuerte")
-elif magnitud>=6 and magnitud<7:
-    print("Muy fuerte")
-elif magnitud>=7:
-    print("Extremo")
+## 10) Utilizando la información aportada en la siguiente tabla sobre las estaciones del año
+## Escribir un programa que pregunte al usuario en cuál hemisferio se encuentra (N/S), qué mes
+## del año es y qué día es. El programa deberá utilizar esa información para imprimir por pantalla
+## si el usuario se encuentra en otoño, invierno, primavera o verano
