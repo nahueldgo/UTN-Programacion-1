33fcb35350f18ed5456dcf7f9701493604b6a67c
Nahuel <nahueldgo@gmail.com>
2025-04-16 13:37:37 -0300

    Guia en proceso

--- 04 Estructuras Repetitivas/Ejercicio_06.py
+++ 04 Estructuras Repetitivas/Ejercicio_06.py
@@ -1,4 +1,5 @@
 ##6) Desarrolla un programa que imprima en pantalla todos los números pares comprendidos entre 0 y 100, en orden decreciente.
 
+print("Numeros pares comprendidos entre 0 y 100, en orden decreciente")
 for i in range (100, -2, -2):
     print(i)
