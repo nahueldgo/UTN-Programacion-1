44e17065c379bc726d02a20f99c10168fc3b0a6e
Nahuel <nahueldgo@gmail.com>
2025-04-18 13:58:01 -0300

    Guia terminada

---
+++ 04 Estructuras Repetitivas/Ejercicio_10.py
@@ -0,0 +1,6 @@
+##10) Escribe un programa que invierta el orden de los dígitos de un número ingresado por el usuario.
+## Ejemplo: si el usuario ingresa 547, el programa debe mostrar 745.
+
+numero = input("Ingrese un número: ")
+invertido = numero[::-1]
+print(f"Número invertido: {invertido}")
